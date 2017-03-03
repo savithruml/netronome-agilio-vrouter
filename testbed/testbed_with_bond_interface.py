9ee3e21896fe44009559d07d0854cabfe01e08a1
Savvy_Through <savithruml@users.noreply.github.com>
2017-03-03 12:47:57 -0800

    Update testbed_with_bond_interface.py

--- testbed/testbed_with_bond_interface.py
+++ testbed/testbed_with_bond_interface.py
@@ -2,13 +2,13 @@
 
 # Author: SAVITHRU LOKANATH
 # Contact: SAVITHRU AT JUNIPER.NET
-# Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+# Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
 
 from fabric.api import env
 
-host1 = 'root@192.168.1.1'
-host2 = 'root@192.168.1.2'
-host3 = 'root@192.168.1.3'
+controller = 'root@192.168.1.1'
+compute1 = 'root@192.168.1.2'
+compute2 = 'root@192.168.1.3'
 
 ext_routers = []
 
@@ -17,48 +17,49 @@ router_asn = 64512
 host_build = 'root@192.168.1.1'
 
 env.roledefs = {
-    'all': [host1,host2,host3],
-    'cfgm': [host1],
-    'openstack': [host1],
-    'control': [host1],
-    'compute': [host1,host2],
-    'collector': [host1],
-    'webui': [host1],
-    'database': [host1],
+    'all': [controller,compute1,compute2],
+    'cfgm': [controller],
+    'openstack': [controller],
+    'control': [controller],
+    'compute': [compute1,compute2],
+    'collector': [controller],
+    'webui': [controller],
+    'database': [controller],
     'build': [host_build],
-    'storage-master': [host1],
-    'storage-compute': [host1],
+    'storage-master': [controller],
+    'storage-compute': [controller],
 }
 
 env.openstack_admin_password = 'secret123'
 
 env.hostnames = {
-    host1: 'controller',
-    host2: 'compute-1',
-    host3: 'compute-2',
+    controller: 'controller',
+    compute1: 'compute1',
+    compute2: 'compute2',
 }
 
 env.passwords = {
-    host1: 'secret123',
-    host2: 'secret123',
-    host3: 'secret123',
+    controller: 'secret123',
+    compute1: 'secret123',
+    compute2: 'secret123',
     host_build: 'secret123',
 }
 
 minimum_diskGB = 50
 
 bond= {
-    host2 : { 'name': 'bond0', 'member': ['nfp_p0','nfp_p1'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
-    host3 : { 'name': 'bond0', 'member': ['nfp_p0','nfp_p1'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
+    compute1 : { 'name': 'bond0', 'member': ['nfp_p0','nfp_p1','nfp_p2','nfp_p3'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
+    compute2 : { 'name': 'bond0', 'member': ['nfp_p0','nfp_p1','nfp_p2','nfp_p3'], 'mode': '802.3ad', 'xmit_hash_policy': 'layer3+4' },
 }
 
+
 control_data = {
-    host1 : { 'ip': '172.31.255.1/24', 'gw' : '172.31.255.1', 'device': 'eth1' },
-    host2 : { 'ip': '172.31.255.2/24', 'gw' : '172.31.255.1', 'device': 'bond0' },
-    host3 : { 'ip': '172.31.255.3/24', 'gw' : '172.31.255.1', 'device': 'bond0' },
+    controller : { 'ip': '172.31.255.1/24', 'gw' : '172.31.255.1', 'device': 'eth1' },
+    compute1 : { 'ip': '172.31.255.2/24', 'gw' : '172.31.255.1', 'device': 'bond0' },
+    compute2 : { 'ip': '172.31.255.3/24', 'gw' : '172.31.255.1', 'device': 'bond0' },
 }
 
 env.ns_agilio_vrouter = {
-    host2: {'huge_page_alloc': '24G', 'huge_page_size': '1G', 'coremask': '2,4', 'pinning_mode': 'auto:split'},
-    host3: {'huge_page_alloc': '24G', 'huge_page_size': '1G', 'coremask': '2,4', 'pinning_mode': 'auto:split'}
+    compute1: {'huge_page_alloc': '24G', 'huge_page_size': '1G', 'coremask': '2,4', 'pinning_mode': 'auto:split'},
+    compute2: {'huge_page_alloc': '24G', 'huge_page_size': '1G', 'coremask': '2,4', 'pinning_mode': 'auto:split'}
 }
